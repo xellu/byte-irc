2e18d8fdce1c513438ab7db5c30580fa9ff6e7c6
Xellu <83588955+xellu@users.noreply.github.com>
2024-03-05 14:55:40 +0700

    base

---
+++ engine/client/__init__.py
@@ -0,0 +1,7 @@
+
+class ClientManager:
+    def __init__(self):
+        self.status = "Idle"
+        
+        
+Client = ClientManager()
--- engine/pages/connect.py
+++ engine/pages/connect.py
@@ -2,16 +2,41 @@ from photon import Page
 from photon.theme import Variants
 from photon.components import Input
 
+
 class Connect(Page):
     def __init__(self, app):
         self.app = app
-        self.input = Input(app, "Server IP", width=20, callback=self.server_submit, auto_render=False)
+        
+        self.stage = "server"
+        
+        self.ip = ""
+        self.user = ""
+        
+        self.server_address = Input(app, "Server Address", width=30, callback=self.server_submit, auto_render=False)
+        self.username = Input(app, "Username", width=30, callback=self.username_submit, auto_render=False)
         
     def on_render(self, sc):
-        self.input.on_render(sc)
+        match self.stage:
+            case "server":
+                self.server_address.on_render(sc)
+            case "username":
+                self.username.on_render(sc)
         
     def on_input(self, key):
-        self.input.on_input(key)
+        match self.stage:
+            case "server":
+                self.server_address.on_input(key)
+            case "username":
+                self.username.on_input(key)
         
     def server_submit(self, value):
-        print(value)
+        if value.replace(" ", "") != "":
+            self.ip = value
+            
+        self.stage = "username"
+        
+    def username_submit(self, value):
+        if value.replace(" ", "") != "":
+            self.user = value
+        
+        
